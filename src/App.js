80b90e65c179ee57906664fab2f6d29b8aa0e630
James McEwan <james@mcewan.io>
2018-03-11 09:07:01 +0000

    inital styling wireframes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,58 @@
 import React from 'react';
 
-const App = () => <h1>Bootstrapped</h1>;
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="weather-app">
+        <h1>Edinburgh GB</h1>
+        <div className="current-date">
+          <h2>Sunday 11 March 2018</h2>
+          <div className="current-date__data">
+            <h3>Current Forcast: Rain - light rain</h3>
+            <p>Icon</p>
+            <h3>Temperature (c)</h3>
+            <ul>
+              <li>Average: 281.15</li>
+              <li>Min: 281.15</li>
+              <li>Max: 281.15</li>
+            </ul>
+          </div>
+        </div>
+        <nav className="forcast">
+          <button className="forcast__day forcast__day--selected">
+            Sun
+            <img src="icons/01d.png" alt="" />
+          </button>
+
+          <button className="forcast__day">
+            Sun
+            <img src="icons/01d.png" alt="" />
+          </button>
+
+          <button className="forcast__day">
+            Sun
+            <img src="icons/01d.png" alt="" />
+          </button>
+
+          <button className="forcast__day">
+            Sun
+            <img src="icons/01d.png" alt="" />
+          </button>
+
+          <button className="forcast__day">
+            Sun
+            <img src="icons/01d.png" alt="" />
+          </button>
+        </nav>
+      </div>
+    );
+  }
+}
 
 export default App;
--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const CleanWebPackPlugin = require('clean-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -8,6 +9,10 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
       {
         test: /\.html$/,
         use: 'html-loader'
@@ -20,6 +25,9 @@ module.exports = {
   },
   plugins: [
     new CleanWebPackPlugin(['dist']),
+    new CopyWebpackPlugin([
+      { from: './src/static/icons/**', to: 'icons', flatten: true }
+    ]),
     new HtmlWebPackPlugin({
       template: './src/static/index.html'
     })
