90597b0d1ffd5976f7f3cca2ed52d32a50db091a
James McEwan <james@mcewan.io>
2018-03-11 10:22:56 +0000

    split into components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,63 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
 import data from './JSONStructure';
 import getData from './data';
+
+const Header = ({ name, country }) => (
+  <h1>
+    {name}, {country}
+  </h1>
+);
+
+Header.propTypes = {
+  name: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired
+};
+
+const CurrentForcast = ({ main, description }) => (
+  <h3>
+    Current Forcast: {main} - {description}
+  </h3>
+);
+
+const CurrentTemperature = ({ temp, temp_min, temp_max }) => (
+  <Fragment>
+    <h3>Temperature (c)</h3>
+    <ul>
+      <li>Average: {temp}</li>
+      <li>Min: {temp_min}</li>
+      <li>Max: {temp_max}</li>
+    </ul>
+  </Fragment>
+);
+
+const CurrentDay = ({ dt, dt_txt, children }) => (
+  <div key={`current-${dt}`} className="current-date">
+    <h2>{dt_txt}</h2>
+    <div className="current-date__data">{children}</div>
+  </div>
+);
+
+CurrentDay.propTypes = {
+  dt: PropTypes.number.isRequired,
+  dt_txt: PropTypes.string.isRequired
+};
+
+const ForcastDay = ({ dt, dt_txt, children }) => (
+  <button key={`forcast-${dt}`} className="forcast__day forcast__day--selected">
+    {dt_txt}
+    {children}
+  </button>
+);
+
+const ForcastImage = ({ icon, alt }) => (
+  <img src={`icons/${icon}.png`} alt={alt} />
+);
+
+CurrentDay.propTypes = {
+  dt: PropTypes.number.isRequired,
+  dt_txt: PropTypes.string.isRequired
+};
 
 class App extends React.Component {
   constructor() {
@@ -28,34 +85,21 @@ class App extends React.Component {
   render() {
     return (
       <div className="weather-app">
-        <h1>
-          {this.state.data.city.name}, {this.state.data.city.country}
-        </h1>
+        <Header
+          name={this.state.data.city.name}
+          country={this.state.data.city.country}
+        />
         {this.state.data.list.map(day => (
-          <div key={day.dt} className="current-date">
-            <h2>{day.dt_txt}</h2>
-            <div className="current-date__data">
-              <h3>
-                Current Forcast: {day.weather.main} - {day.weather.description}
-              </h3>
-              <h3>Temperature (c)</h3>
-              <ul>
-                <li>Average: {day.main.temp}</li>
-                <li>Min: {day.main.temp_min}</li>
-                <li>Max: {day.main.temp_max}</li>
-              </ul>
-            </div>
-          </div>
+          <CurrentDay {...day}>
+            <CurrentForcast {...day.weather[0]} />
+            <CurrentTemperature {...day.main} />
+          </CurrentDay>
         ))}
         <nav className="forcast">
           {this.state.data.list.map(day => (
-            <button
-              key={day.dt}
-              className="forcast__day forcast__day--selected"
-            >
-              {day.dt_txt}
-              <img src={`icons/${day.weather[0].icon}.png`} alt={day.dt_txt} />
-            </button>
+            <ForcastDay {...day}>
+              <ForcastImage icon={day.weather[0].icon} alt={day.dt_txt} />
+            </ForcastDay>
           ))}
         </nav>
       </div>
