d76dc9349aec355105858fc95ef1672f23bcb8d7
James McEwan <60427+jamesmcewan@users.noreply.github.com>
2020-02-09 14:06:39 +0000

    use hooks

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components/macro';
 import forcastData from '../../data/forcastData';
 import Header from '../Header/Header';
@@ -10,76 +10,56 @@ import Box from '../styles/Box';
 import theme from '../styles/theme';
 import GlobalStyle from '../styles/GlobalStyle';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+const App = () => {
+  const [dates, setDates] = useState({});
+  const [city, setCity] = useState({});
+  const [currentDay, setCurrentDay] = useState([]);
+  const [error, setError] = useState('');
 
-    this.state = {
-      dates: {},
-      city: {},
-      currentDay: [],
-      error: ''
-    };
-
-    this.changeDay = this.changeDay.bind(this);
-    this.changeLocale = this.changeLocale.bind(this);
-  }
-
-  componentWillshallow() {
-    this.getforcastData('Edinburgh, GB');
-  }
-
-  getforcastData(loc) {
+  const getforcastData = loc => {
     forcastData(loc)
       .then(response => {
         if (response.cod !== '200') {
-          return this.setState({ error: response.message });
+          setError(response.message);
+          return;
         }
-
-        return this.setState({
-          city: response.city,
-          dates: response.dates,
-          currentDay: response.dates[0]
-        });
+        setCity(response.city);
+        setDates(response.dates);
+        setCurrentDay(response.dates[0]);
       })
-      .catch(reason => this.setState({ error: reason }));
-  }
+      .catch(reason => setError(reason));
+  };
 
-  changeDay(day) {
-    const newDay = [...this.state.dates[day]];
+  const changeDay = day => {
+    const newDay = [...dates[day]];
+    setCurrentDay(newDay);
+  };
 
-    this.setState({
-      currentDay: newDay
-    });
-  }
-
-  changeLocale(e) {
+  const changeLocale = e => {
     const newLocale = e.target.value.replace(' ', '');
-    this.getforcastData(newLocale);
-  }
+    getforcastData(newLocale);
+  };
 
-  render() {
-    return (
-      <>
-        <ThemeProvider theme={theme}>
-          <GlobalStyle />
-          <Box maxWidth="90%" m="auto" data-selector="weather-app">
-            <LocaleInput
-              {...this.state.city}
-              error={this.state.error}
-              changeLocale={this.changeLocale}
-            />
-            <Header {...this.state.city} />
+  useEffect(() => {
+    getforcastData('Edinburgh, GB');
+  }, []);
 
-            <Currently {...this.state.currentDay[0]} />
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Box maxWidth="90%" m="auto" data-selector="weather-app">
+          <LocaleInput {...city} error={error} changeLocale={changeLocale} />
+          <Header {...city} />
 
-            <Today cd={this.state.currentDay} />
-          </Box>
-          <Forcasts dates={this.state.dates} changeDay={this.changeDay} />
-        </ThemeProvider>
-      </>
-    );
-  }
-}
+          <Currently {...currentDay[0]} />
+
+          <Today cd={currentDay} />
+        </Box>
+        <Forcasts dates={dates} changeDay={changeDay} />
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
